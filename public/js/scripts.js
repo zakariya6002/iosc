367d7baece9f5e700800292c6315fffa4942cf08
zakariya6002 <zhassan6002@gmail.com>
2022-01-11 20:18:31 +0200

    updated

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,3 +5,14 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function createbubble(options) {
+    const section = document.querySelector('section');
+    const createElement = document.createElement('div');
+    var size = math.random() * 60;
+
+    createElement.style.width = size + 'px';
+    createElement.style.height = size + 'px';
+    createElement.style.left = math.random() + 'px';
+
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
 <body class="d-flex flex-column h-100">
     <main class="flex-shrink-0">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-5">
                 <a class="navbar-brand" href="#">
                     <img src="{{url('assets/logo.png')}}" alt="..." height="60">
@@ -35,13 +35,13 @@
             </div>
         </nav>
         <!-- Header-->
-        <header class="bg-dark py-5">
+        <header class="bg-light py-5">
             <div class="container px-3">
                 <div class="row gx-5 align-items-center justify-content-center">
                     <div class="col-lg-8 col-xl-7 col-xxl-6">
                         <div class="my-5 text-center text-xl-start">
-                            <h1 class="display-3 fw-bolder text-white mb-2">Indian Ocean SeaFood Company</h1>
-                            <p class="lead fw-normal text-white-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                            <h1 class="display-3 fw-bolder text-primary mb-2">Indian Ocean SeaFood Company</h1>
+                            <p class="lead fw-normal text-black-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
                             <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
                                 <a class="btn btn-danger btn-lg px-4 me-sm-3" href="#features">Get Started</a>
                                 <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
